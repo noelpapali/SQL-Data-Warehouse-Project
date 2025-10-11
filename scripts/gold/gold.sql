e19dd85159e426f0ad0cc68bb20c12cc18a7fe46
Noel Gerald Papali <noel.papali@utdallas.edu>
2025-10-10 22:05:38 -0500

    Add files via upload

---
+++ scripts/gold/gold.sql
@@ -0,0 +1,63 @@
+
+GO
+-- creating dim customers
+CREATE VIEW gold.dim_customers AS
+SELECT 
+	ROW_NUMBER() OVER(ORDER BY cst_id) AS customer_key,  -- surrogate key
+	cst_id AS customer_id,
+	cst_key AS customer_number,
+	cst_firstname AS first_name,
+	cst_lastname AS last_name,
+	la.cntry AS country,
+	cst_marital_status AS marital_status,
+	CASE WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr --CRM is the master for gender info
+	ELSE COALESCE(ca.gen, 'n/a')
+	END AS gender,
+	ca.bdate AS birth_date,
+	ci.cst_created_date AS create_date	
+	FROM silver.crm_cust_info ci
+LEFT JOIN silver.erp_cust_az12 ca
+ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 la
+ON ci.cst_key = la.cid;
+
+--creating dim products
+GO
+CREATE VIEW gold.dim_products AS
+SELECT
+	ROW_NUMBER() OVER(ORDER BY pn.prd_start_date, pn.prd_key) AS product_key,  -- surrogate key
+	prd_id AS product_id,
+	prd_key AS product_number,
+	prd_name AS product_name,
+	cat_id AS category_id,
+	pc.cat AS category,
+	pc.subcat AS subcategory,
+	pc.maintenance AS maintenance,
+	prd_cost AS cost,
+	prd_line AS product_line,
+	prd_start_date as start_date
+FROM silver.crm_prd_info pn
+LEFT JOIN silver.erp_px_cat_g1v2 pc
+ON pn.cat_id = pc.id
+WHERE pn.prd_end_date IS NULL;   -- filter out historical data
+
+
+-- creating fact sales table
+GO
+CREATE VIEW gold.fact_sales AS
+SELECT
+sls_ord_num AS order_number,
+cu.customer_key,
+pr.product_key,
+sls_order_date AS order_date,
+sls_ship_date AS ship_date,
+sls_due_date AS due_date,
+sls_sales AS sales_amount,
+sls_quantity AS quantity,
+sls_price AS price
+FROM silver.crm_sale_details sd
+LEFT JOIN gold.dim_products pr ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customers cu ON sd.sls_cust_id = cu.customer_id
+
+
+ 
